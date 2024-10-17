8c2543f729b8038cfd041933b0c4366afbc36773
KaukabFarrukh <144961941+KaukabFarrukh@users.noreply.github.com>
2024-10-17 11:52:42 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//Exercise 1
+console.log("Hello,World!");
+
+//Exercise 2
+let name = "Sara";
+let age = 24;
+let favourite color = Blue;
+console.log("My name is" + name + ", I am" + age + "years old and my favourite color is" + favourite color + ".");
+
+//Exercise 3 
+let num1 = 10;
+let num2 = 5;
+console.log("Addition: " + (num1 + num2));
+console.log("Subtraction: " + (num2  - num1));
+console.log("Multiplication: " + (num1 * num2));
+console.log("Division: " + (num2 / num1));
